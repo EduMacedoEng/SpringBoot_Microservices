a03121d852193e7dd778d087091b85a8cb24ea5d
Eduardo Macedo <eduardomacedo.eng@hotmail.com>
2021-05-13 23:44:37 -0300

    Configuring Eureka Clients

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -2,10 +2,13 @@ package com.devsuperior.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "hr-worker")
+//@RibbonClient(name = "hr-worker")
+// O Eureka já tem o balanceamento de carga implementado então não é mais necessário usar o Ribbon
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {

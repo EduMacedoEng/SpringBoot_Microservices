0a8a33fff49589cf6ce97db925e06c83158c9d66
Eduardo Macedo <eduardomacedo.eng@hotmail.com>
2021-05-17 01:58:15 -0300

    Hysterix fault tolerance

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -2,12 +2,14 @@ package com.devsuperior.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //@RibbonClient(name = "hr-worker")
 // O Eureka já tem o balanceamento de carga implementado então não é mais necessário usar o Ribbon
+@EnableCircuitBreaker
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -35,6 +35,12 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		/* Aqui eu fiz um teste baseado no tempo do Hysterix e Riborn
+		 * // int x = 1; // if (x == 1) // throw new RuntimeException("Test"); try {
+		 * Thread.sleep(3000L); } catch (InterruptedException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); }
+		 */
+		
 		log.info("PORT: " + env.getProperty("local.server.port"));
 		
 		Worker obj = workerRepository.findById(id).get();
